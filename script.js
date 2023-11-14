8fe36013520983a234cdce774f918681fccc1e5b
Jacob kaufenberg <Jacob.kaufenberg@icloud.com>
2023-11-14 13:38:30 -0600

    added the 8-128 number requierment

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -14,41 +15,75 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-function generatePassword(length, numbers, uppercase, lowercase, symbols) {
-var characters = '';
 
-var result = prompt('How many characters would you like your password to contain?');{
+function generatePassword() {
 
-console.log(result);
+// 1. need to prompt the user for the password criteria 
+//  a. password length between 8-128 
+//  b. lowercase uppercase, numbers and special characters. 
+var passwordLength = prompt('Between 8-128 how many characters would you like your password to be?');
+passwordLength = parseInt(passwordLength);
+if (passwordLength < 8 || passwordLength > 128){
+  alert('Please enter a valid number!');
 }
 
-var result = confirm( 'click ok to confirm Include special characters' ); {
+confirm( 'click ok to confirm Include special characters' )
+
+confirm( 'click ok to confirm Include numeric characters' )
+
+confirm( 'click ok to confirm Include lowercase characters' )
+
+confirm( 'click ok to confirm Include uppercase characters' )
+
+
+
+
+// 2. Validate the input. 
+// 3. display the password on the page(return of generate password)
+
+
+
+
+
+
+  return ('generated password goes here')
+}
+//function generatePassword(length, numbers, uppercase, lowercase, symbols) {
+// var characters = '';
+
+ /* var passwordAmount = prompt('Between 8-128 how many characters would you like your password to be?'); {
+if(passwordAmount > '8') {
+  alert('Please enter a number between 8-128')
+}
+}
+
+var specialChar = confirm( 'click ok to confirm Include special characters' ); {
   
-  console.log(result); 
+  console.log(specialChar); 
   if (symbols) {
     characters +=  '!#$%&?@~'
   }
 }
 
-var result = confirm( 'click ok to confirm Include numeric characters' ); {
+var numChar = confirm( 'click ok to confirm Include numeric characters' ); {
   
-  console.log(result); 
+  console.log(numChar); 
   if (numbers) {
     characters +=  '0123456789'
 }
 }
 
-var result = confirm( 'click ok to confirm Include lowercase characters' ); {
+var lowercaseChar = confirm( 'click ok to confirm Include lowercase characters' ); {
   
-  console.log(result); 
+  console.log(lowercaseChar); 
   if (lowercase) {
     characters +=  'abcdefghijklmnopqrstuvwxyz'
   }
 }
 
-var result = confirm( 'click ok to confirm Include uppercase characters' ); {
+var uppercaseChar = confirm( 'click ok to confirm Include uppercase characters' ); {
   
-  console.log(result); 
+  console.log(uppercaseChar); 
   if (uppercase) {
     characters += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
   }
@@ -61,4 +96,4 @@ while(passwordArray.length < length){
 
 return 'new password';
 
-}
+ } */ 
